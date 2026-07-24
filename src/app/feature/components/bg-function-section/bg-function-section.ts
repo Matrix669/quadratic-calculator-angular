953c74416a52e8cef279c4b3af31b3f383ab9ebe
MaksT <mtkaczykn@gmail.com>
2026-07-25 00:20:01 +0200

    create bg section which showing chart

---
+++ src/app/feature/components/bg-function-section/bg-function-section.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bg-function-section',
+  imports: [],
+  templateUrl: './bg-function-section.html',
+  styleUrl: './bg-function-section.scss',
+})
+export class BgFunctionSection {}
