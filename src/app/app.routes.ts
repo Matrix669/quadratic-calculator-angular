6c0ccaa91df86448934821b9782c1ba3bb8fb3c2
MaksT <mtkaczykn@gmail.com>
2026-07-26 11:57:47 +0200

    change structure of folders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { HomePage } from '@/app/feature/home.page';
+import { HomePage } from '@/app/feature/home/home.page';
 import { App } from '@/app/app';
 import { Routes } from '@angular/router';
 
