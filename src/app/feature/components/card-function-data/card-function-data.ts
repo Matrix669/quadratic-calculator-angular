3c0c01cc4faf1a6c0a8ae75233864dc390021530
MaksT <mtkaczykn@gmail.com>
2026-07-26 21:14:19 +0200

    make validation for first, second and third value

--- src/app/feature/components/card-function-data/card-function-data.ts
+++ src/app/feature/components/card-function-data/card-function-data.ts
@@ -26,25 +26,53 @@ export class CardFunctionData {
   secondValue = signal<number | null>(null);
   thirdValue = signal<number | null>(null);
 
+  firstError = signal<string | null>(null);
+  secondError = signal<string | null>(null);
+  thirdError = signal<string | null>(null);
   onFirstInput(event: Event): void {
     const value = (event.target as HTMLInputElement).value;
     this.firstValue.set(value === '' ? null : Number(value));
+
+    if (this.firstValue() !== null && this.firstValue() !== 0) {
+      this.firstError.set(null);
+    }
   }
   onSecondInput(event: Event): void {
     const value = (event.target as HTMLInputElement).value;
     this.secondValue.set(value === '' ? null : Number(value));
+
+    if (this.secondValue() !== null) {
+      this.secondError.set(null);
+    }
   }
   onThirdInput(event: Event): void {
     const value = (event.target as HTMLInputElement).value;
     this.thirdValue.set(value === '' ? null : Number(value));
+
+    if (this.thirdValue() !== null) {
+      this.thirdError.set(null);
+    }
   }
 
   onCalculate(): void {
-    // console.log({
-    //   first: this.firstValue(),
-    //   second: this.secondValue(),
-    //   third: this.thirdValue(),
-    // })
+    this.firstError.set(null);
+    this.secondError.set(null);
+    this.thirdError.set(null);
+
+    if (this.firstValue() === null) {
+      this.firstError.set('To pole jest wymagane');
+    } else if (this.firstValue() === 0) {
+      this.firstError.set("'a' nie moze być równe 0!");
+    }
+    if (this.secondValue() === null) {
+      this.secondError.set('To pole jest wymagane');
+    }
+    if (this.thirdValue() === null) {
+      this.thirdError.set('To pole jest wymagane');
+    }
+    if (this.firstError() || this.secondError() || this.thirdError()) {
+      return;
+    }
     this.calculate.emit({
       first: this.firstValue(),
       second: this.secondValue(),
